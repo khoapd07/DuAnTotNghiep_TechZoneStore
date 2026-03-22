44ee345bd90ef0e65f5fe8ae90ac46f20f458c4e
dungnoname <dung9012@gmail.com>
2026-03-22 23:06:27 +0700

    Update ProductsVariantDAO

---
+++ Backend/src/main/java/com/poly/backend/dao/ProductVariantDAO.java
@@ -0,0 +1,10 @@
+package com.poly.backend.dao;
+
+import com.poly.backend.entity.ProductVariant;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductVariantDAO extends JpaRepository<ProductVariant, Integer> {
+    // Để trống thế này là Spring Boot tự động hiểu và cấp sẵn các hàm tìm kiếm (findById)
+}
